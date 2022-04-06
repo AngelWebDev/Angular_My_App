75d2ba68c51a628452c83cdd3510ac1e40cc1ce6
angelwebdev <web.angel1993@gmail.com>
2022-04-06 08:41:37 -0400

    add navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,21 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock_heros';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(public messageService: MessageService) {}
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id= ${id}`);
+    return of(hero);
   }
 }
